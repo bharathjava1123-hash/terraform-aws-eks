89ad91cbcf40992249a8fea7b9d05f04895eb16b
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-08-04 13:03:10 +0530

    k8

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -8,10 +8,10 @@ terraform {
 
   backend "s3" {
 
-    bucket = "terraform-k8s"
+    bucket = "terraform-test11"
     key    = "expense-vpc"
     region = "us-east-1"
-    dynamodb_table = "expesne-locking"
+    dynamodb_table = "terraform-test11"
   
   }
 
